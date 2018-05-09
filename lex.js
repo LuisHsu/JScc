6a1ec710753a1c461f4d39029c12f599e8e53481
LuisHsu <luis8404272@gmail.com>
2018-05-09 23:59:16 +0800

    [CC] Decimal floating

--- lex.js
+++ lex.js
@@ -52,6 +52,8 @@ class Lexer extends Transform{
 		this.dataStr = this.dataStr.trim();
 		if(
 			(token = this.tokString())||
+			(token = this.tokCharacter())||
+			(token = this.tokFloat())||
 			(token = this.tokKeyword())||
 			(token = this.tokIdentifier())||
 			(token = this.tokPunctuator())
@@ -109,7 +111,44 @@ class Lexer extends Transform{
 	}
 
 	tokFloat(){
-
+		var regex = /^(0[xX](\.[\wA-Fa-f]+|[\wA-Fa-f]+\.?[\wA-Fa-f]*)[pP][\+\-]?\w+|((\.\d+|\d+\.\d*)([eE][\+\-]?\d+)?|\d+[eE][\+\-]?\d+))[flFL]?/;
+		var matched = regex.exec(this.dataStr);
+		if(matched){
+			this.dataStr = this.dataStr.substr(matched[0].length);
+			var val = 0.0;
+			var double = true;
+			if(matched[0].startsWith("0x") || matched[0].startsWith("0X")){
+				// hexadecimal
+				var hexadecimal = matched[2].split(".");
+				val += parseInt(hexadecimal[0], 16);
+				if(hexadecimal[1]){
+					for(var i = 0; i < hexadecimal[1].length; ++i){
+						val += parseInt(hexadecimal[1].charAt(i), 16) * Math.pow(0.0625,i+1);
+					}
+				}
+				if(matched[0].split(/[pP]/)[1]){
+					val *= Math.pow(2, parseInt(matched[0].split(/[pP]/)[1]));
+				}
+			}else{
+				// decimal
+				var decimal = matched[0].split(/[eE]/);
+				val += parseFloat(decimal[0]);
+				if(decimal[1]){
+					val *= Math.pow(10, parseInt(decimal[1]));
+				}
+			}
+			// Suffix
+			if(matched[0].endsWith('f') || matched[0].endsWith('F')){
+				double = false;
+			}
+			return {
+				type: 'floating',
+				value: val,
+				double: double
+			};
+		}else{
+			return null;
+		}
 	}
 
 	tokInteger(){
@@ -117,7 +156,63 @@ class Lexer extends Transform{
 	}
 
 	tokCharacter(){
-
+		var regex = /^(u|U|L)?\'(\\\'|[^\"\n])*\'/;
+		var matched = regex.exec(this.dataStr);
+		if(matched){
+			regex = /^(\\(x[\dA-Fa-f]+|u[\dA-Fa-f]{4}|U[\dA-Fa-f]{8}|[0-7]{1,3}|[\'\"\?\\abfnrtv])|[\w!\;<#=%>&\?\'\[\(\)\]\*\^\+\,\{-\|\.\}\/~\:])/;
+			this.dataStr = this.dataStr.substr(matched[0].length);
+			var str = "";
+			var charSize = 1;
+			if(matched[0].startsWith("u") || matched[0].startsWith("L")){
+				// UTF-16 string
+				str = matched[0].substr(2, matched[0].length - 3);
+				charSize = 2;
+			}else if(matched[0].startsWith("U")){
+				// UTF-32 string
+				str = matched[0].substr(2, matched[0].length - 3);
+				charSize = 4;
+			}else{
+				// Normal string
+				str = matched[0].substr(1, matched[0].length - 2);
+				charSize = 1;
+			}
+			var character = regex.exec(str);
+			if(character != null){
+				var newBuf = Buffer.alloc(charSize);
+				character = character[0];
+				var chVal = 0;
+				if(character.startsWith("\\x")||character.startsWith("\\u")||character.startsWith("\\U")){
+					chVal = parseInt(character.substr(2), 16);
+				}else if(character.startsWith("\\")){
+					chVal = parseInt(character.substr(1), 8);
+				}else{
+					chVal = character.charCodeAt(0);
+				}
+				switch(charSize){
+					case 4:
+						newBuf.writeUInt32LE(chVal);
+					break;
+					case 2:
+						newBuf.writeUInt16LE(chVal);
+					break;
+					default:
+						newBuf.writeUInt8(chVal);
+					break;
+				}
+				return {
+					type: 'character',
+					value: newBuf,
+					size: charSize
+				};
+			}
+			return {
+				type: 'character',
+				value: Buffer.alloc(charSize),
+				size: charSize
+			};
+		}else{
+			return null;
+		}
 	}
 
 	tokString(){
@@ -149,7 +244,7 @@ class Lexer extends Transform{
 			for(var character = regex.exec(str); character != null; character = regex.exec(str)){
 				var newBuf = Buffer.alloc(charSize);
 				character = character[0];
-				var chVal = "";
+				var chVal = 0;
 				if(character.startsWith("\\x")||character.startsWith("\\u")||character.startsWith("\\U")){
 					chVal = parseInt(character.substr(2), 16);
 				}else if(character.startsWith("\\")){
@@ -172,7 +267,8 @@ class Lexer extends Transform{
 			}
 			return {
 				type: 'string',
-				value: Buffer.concat(buffers, charSize * buffers.length)
+				value: Buffer.concat(buffers, charSize * buffers.length),
+				size: charSize
 			};
 		}else{
 			return null;
